cc8245e4482615b692a05e90f21749939e5be9a4
ParthasarathiSwain <rajaswain6969@gmail.com>
2025-04-29 19:06:35 +0530

    Uploading 29-04-2025 Notes

--- Array/Test03.java
+++ Array/Test03.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+
 class Test03{
 	public static void main(String args[]){
 		Scanner sc=new Scanner(System.in);
---
+++ Package/Test02.java
@@ -0,0 +1,8 @@
+package lion;
+import tiger.Test01;
+class Test02{
+	public static void main(String args[]){
+                Test01 obj=new Test01();
+		System.out.println("Bye , Thank You");
+	}
+}
