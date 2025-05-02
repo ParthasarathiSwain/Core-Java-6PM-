1d6a7ae155369347a867cdea977ff186d19eee00
ParthasarathiSwain <rajaswain6969@gmail.com>
2025-05-02 18:47:34 +0530

    Uploading 02-05-2025 Notes

--- Array/Test03.java
+++ Array/Test03.java
@@ -1,7 +1,7 @@
 
 class Test03{
 	public static void main(String args[]){
-		Scanner sc=new Scanner(System.in);
+		java.util.Scanner  sc = new java.util.Scanner(System.in);
 		System.out.println("Enter Size : ");
 		int size=sc.nextInt();
 
---
+++ command Line Args/Test01.java
@@ -0,0 +1,26 @@
+class Test01{
+	public static void main(String a[]){
+		//String s1=a[0];   
+		//int d=Integer.parseInt(s1);            
+		//String s2=a[1];
+		//int b=Integer.parseInt(s2); 
+		
+		//int result=d+b;
+
+		//System.out.println("s1 : "+s1);
+		//System.out.println("s2 : "+s2);
+		//System.out.println("Result : "+result);
+
+		String s1=a[0];
+		float f1=Float.parseFloat(s1);
+		String s2=a[1];
+		float f2=Float.parseFloat(s2);
+		float f3=f1+f2;
+		System.out.println("F3 : "+f3);
+
+		
+	}
+}
+
+
+
