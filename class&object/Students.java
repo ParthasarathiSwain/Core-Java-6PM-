46988d31d418e03cda13995ca66edc33b9b6bf81
ParthasarathiSwain <rajaswain6969@gmail.com>
2025-05-14 12:11:47 +0530

    14-05-2025 notes

--- class&object/Students.java
+++ class&object/Students.java
@@ -1,4 +1,5 @@
 class Students{
+        String schoolName="BBSR SCHOOL";
 	String name;
         String rollNum;
         int age;
@@ -18,12 +19,30 @@ class Students{
 		s1.rollNum="100AB02";
 		s1.age=21;
 		s1.address="BBSR";
-		
+		s1.schoolName="BBSR Sc"; //1mb
+
 		System.out.println("Student 1");
+		System.out.println("School Name    : "+s1.schoolName);
 		System.out.println("Name    : "+s1.name);
 		System.out.println("RollNum : "+s1.rollNum);
 		System.out.println("Age     : "+s1.age);
 		System.out.println("Address : "+s1.address);
+
+
+		Students s2=new Students();
+		s2.name="Rahul Swain";
+		s2.rollNum="100AB03";
+		s2.age=24;
+		s2.address="BDK";
+		s2.schoolName="BBSR Sc"; //1mb
+
+                System.out.println("Student 2");
+		System.out.println("School Name    : "+s2.schoolName);
+		System.out.println("Name    : "+s2.name);
+		System.out.println("RollNum : "+s2.rollNum);
+		System.out.println("Age     : "+s2.age);
+		System.out.println("Address : "+s2.address);
+
 
      }
 }
