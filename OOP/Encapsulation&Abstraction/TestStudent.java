e17c49ce0f21881b7fa0cdc0b9d29b668f739522
ParthasarathiSwain <rajaswain6969@gmail.com>
2025-05-29 12:04:49 +0530

    Uploading 29-05-2025 Notes

---
+++ OOP/Encapsulation&Abstraction/TestStudent.java
@@ -0,0 +1,26 @@
+class TestStudent{
+	public static void main(String args[]){
+	/*	Student s1=new Student();
+
+		s1.printStudent();
+
+		s1.setName("Subha Swain");
+		s1.setRoll("100AB01");
+		s1.setAge(20);
+		s1.setAdd("BBSR");
+
+		s1.printStudent();
+
+		String name=s1.getName();
+		System.out.println(name);
+		System.out.println(s1.getName());
+	
+		new Student().setName("Raja");
+		
+		System.out.println(new Student().getName());
+	*/
+	}
+}
+
+
+
